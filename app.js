b6dbd3ffdc94d5fc2e094b7f7d899c258665849f
gareth0712 <gareth_0712@hotmail.com>
2020-08-03 23:04:46 +0800

    Prepare methods for new user APIs

--- app.js
+++ app.js
@@ -112,9 +112,41 @@ const deleteTour = (req, res) => {
 
   res.status(204).json({
     status: 'success',
-    data: null
-  })
-}
+
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
 
 // app.get('/api/v1/tours', getAllTours);
 // app.get('/api/v1/tours/:id', getTour);
