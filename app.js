5c1bb6eac512fdd3752836631abef76b84f41dfc
gareth0712 <gareth_0712@hotmail.com>
2020-11-08 10:50:55 +0800

    Export app.js to run in server.js for a better file structure

--- app.js
+++ app.js
@@ -38,8 +38,5 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// 4) Start the server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+// 4) Start server in server.js
+module.exports = app;
