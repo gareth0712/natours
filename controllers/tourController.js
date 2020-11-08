0937f064c9b28611a8702991726b80c5a8dd5bff
gareth0712 <gareth_0712@hotmail.com>
2020-11-08 22:09:47 +0800

    Add schema, model and update controllers for for CRUD operation on Tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,98 +1,100 @@
-const fs = require('fs');
+const Tour = require('../models/tourModel');
 
-// Convert the JSON to JS object / an array of JS object
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
 
-exports.checkID = (req, res, next, val) => {
-  if (req.params.id * 1 >= tours.length) {
-    return res.status(404).json({
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (e) {
+    res.status(404).json({
       status: 'fail',
-      message: 'Invalid ID',
+      message: e,
     });
   }
-  next();
 };
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    // Shorthand for findOne()
+    // Tour.findOne({ _id: req.params.id })
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (e) {
+    res.status(404).json({
       status: 'fail',
-      message: 'Missing name or price',
+      message: e,
     });
   }
-  next();
 };
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
-};
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
 
-exports.getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-
-  // if (id >= tours.length) {
-  if (!tour) {
-    return res.status(404).json({
+    res.status(201).json({
+      // 201 stands for created
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({
       status: 'fail',
-      message: 'Invalid ID',
+      message: 'Invalid tour given in the request body. Tour not created.',
     });
   }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tours: tour,
-    },
-  });
 };
 
-exports.createTour = (req, res) => {
-  // console.log(req.body)
+exports.updateTour = async (req, res) => {
+  try {
+    // findByIdAndUpdate method
+    // 1st: id to identify the doc
+    // 2nd: what data should be updated
+    // 3rd: other options: e.g. { new: true } means asking the method to return the updated tour
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
 
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        // 201 stands for created
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (e) {
+    res.status(404).json({
+      status: 'fail',
+      message: e,
+    });
+  }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here>',
-    },
-  });
-};
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (e) {
+    res.status(404).json({
+      status: 'fail',
+      message: e,
+    });
+  }
 };
