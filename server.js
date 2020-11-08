327784fbc38f67ea7e859bed2bc00f601e177cce
gareth0712 <gareth_0712@hotmail.com>
2020-11-08 10:53:56 +0800

    Make use of mongoose to integrate with mongodb on Atlas

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'))
+  .catch((e) => console.log(`Error connecting to database: ${e}`));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
