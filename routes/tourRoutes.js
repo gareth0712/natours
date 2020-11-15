db5d3efcdbb308b56d57e1a51955e0612868c793
gareth0712 <gareth_0712@hotmail.com>
2020-11-15 12:19:52 +0800

    Add aliasing middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
-const { route } = require('./userRoutes');
 // We can also apply destructuring for shorter variable names
 // const { getAllTours, createTour, getTour, updateTour, deleteTour } = require('./../controllers/tourController');
 const router = express.Router();
+
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router
   .route('/')
