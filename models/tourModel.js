6f9915653408a0468a32aee8446ce7fc8a10b338
gareth0712 <gareth_0712@hotmail.com>
2020-11-15 10:58:50 +0800

    Update tour schema to allow it store more information

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,19 +1,57 @@
 const mongoose = require('mongoose');
 
+// We can specify schema type options using objects
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true, // Only work for string -> Remove write spaces in beginning and the end
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    require: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(), // Mongo will auto-convert milliseconds to date
+  },
+  startDates: [Date], // e.g. "2021-03-21" -> Mongo will auto-converted to JS Date; if it can't it will throw an error
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
