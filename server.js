fcd0f97da86fb7b8cc8d1f186590f991e345c052
gareth0712 <gareth_0712@hotmail.com>
2020-12-06 17:41:12 +0800

    Add event listener for uncaught exception

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// Events and events' listeners
+// Unhandled exception: Errors / Bugs occurred in synchoronous code but are not handled anywhere
+// Each time there is uncaught exception somewhere in the application, the process object will emit an object called 'uncaughtException'
+// So we can subscribe to the event 'uncaughtException' and handle it here
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('Uncaught exception. Shutting down server...');
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
