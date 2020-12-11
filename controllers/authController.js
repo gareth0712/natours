4d37314b3bccadb086234cd8d889626d113bbe9b
gareth0712 <gareth_0712@hotmail.com>
2020-12-12 02:11:40 +0800

    Update signup API to make use of jwt and add config.env

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,37 @@
+const crypto = require('crypto');
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const sendEmail = require('../utils/email');
 
-exports.signup = async (req, res) => {
-  const newUser = await User.create(req.body);
+const signToken = (id) => {
+  // secret needs to be 32-char long
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    // We can also use User.save to update the user
+    // Instead of .create(req.body), using the following avoid users set themselves as admin via the req.body
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
   });
+});
+
 };
