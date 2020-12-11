1503248e9a81014b8d36df5122866db302ebf5ab
gareth0712 <gareth_0712@hotmail.com>
2020-12-12 02:07:12 +0800

    Update User schema definition

--- models/userModel.js
+++ models/userModel.js
@@ -15,22 +15,32 @@ const userSchema = new mongoose.Schema({
     validate: [validator.isEmail, 'Please provide a valid email.'],
   },
   photo: String,
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
   password: {
     type: String,
     required: [true, 'Please provide a password'],
-    minlength: 8,
+    minlength: [8, 'Password is shorter than the minimum allowed length(8)'],
+    select: false, // Never show password when queried
   },
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
-    minlength: 8,
+    minlength: [8, 'Password is shorter than the minimum allowed length(8)'],
     validate: {
-      // This only works on user.save / user.create, i.e. only when new object is created; won't work on update password
+      // This only works on user.save() / user.create(), i.e. only when new object is created; won't work on update, i.e. findByIdAndUpdate
       validator: function (el) {
         return el === this.password;
       },
+      message: 'Passwords are not the same ',
     },
   },
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
 });
 
 const User = mongoose.model('User', userSchema);
