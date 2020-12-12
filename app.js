8843cf178c1fefc16b0bac782a797384953c1704
gareth0712 <gareth_0712@hotmail.com>
2020-12-12 22:58:32 +0800

    Add helmet for secure http request header and tidy up middleware stack

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -11,11 +12,15 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) GLOBAL Middlewares
-// For logging
+// Set security HTTP headers
+app.use(helmet());
+
+// For dev logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same IP
 // 100 requests from the same ip in an hour
 // crashing will reset the limit
 const limiter = rateLimit({
@@ -23,23 +28,23 @@ const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour',
 });
-
 // Only effective for routes that starts with api
 app.use('/api', limiter);
 
-// For access of req.body
-app.use(express.json());
+// Body parser, reading data from body and store into req.body
+// and limit the req body to 10kb or it will reject the request body
+app.use(express.json({ limit: '10kb' }));
 
 // Serve static files
 app.use(express.static(`${__dirname}/public`));
 
-// For custom middleware
+// For showing request headers
 app.use((req, res, next) => {
   console.log(req.headers);
   next();
 });
 
-// For showing date and time of request
+// For marking date and time of request
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
