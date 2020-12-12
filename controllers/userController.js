aaef60a317554c7e7a45695002b7a830722e43ae
gareth0712 <gareth_0712@hotmail.com>
2020-12-12 21:51:32 +0800

    Add updateMe API so that user can update their name and email

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,15 @@
 const catchAsync = require('../utils/catchAsync');
 const Users = require('../models/userModel');
+const AppError = require('../utils/appError');
+const User = require('../models/userModel');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await Users.find();
@@ -13,7 +23,32 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUser = (req, res) => {
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password update. Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+
+  // 2) Filter out fields that are not allowed to update
+  const filteredBody = filterObj(req.body, 'name', 'email'); // Add the field we want to keep here and filter out all the rest
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true, // Return the updated object
+    runValidators: true, // e.g. check the email address is valid, not checking password as those belongs to authentication controller
+  });
+
+  res.status(200).json({
+    status: 'success',
+    user: updatedUser,
+  });
+});
+
   res.status(500).json({
     status: 'error',
     message: 'This route is not yet defined',
