0e4dc5e4b6f40b63fe300c0c69c75039ed3ef0e1
gareth0712 <gareth_0712@hotmail.com>
2020-12-22 00:57:09 +0800

    update reviewControllers for handling /tours/tourId/reviews routes

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -5,6 +5,7 @@ const Review = require('../models/reviewModel');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
   let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
 
   const reviews = await Review.find(filter);
 
@@ -33,7 +34,10 @@ exports.getReview = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
-  const newReview = await Reviews.create(req.body);
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id; // We get req.user from the protect middleware
+  const newReview = await Review.create(req.body);
 
   res.status(201).json({
     status: 'success',
