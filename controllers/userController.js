696f734876e95f5b8327e21f1f7898d9c960c4cf
gareth0712 <gareth_0712@hotmail.com>
2020-12-25 02:25:50 +0800

    add create user to be performed by admin

--- controllers/userController.js
+++ controllers/userController.js
@@ -56,5 +56,6 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
 exports.getUser = factory.getOne(User);
 exports.getAllUsers = factory.getAll(User);
 // Do NOT update passwords with this
+exports.createUser = factory.createOne(User);
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
