6b1848d8e92abc1f68919ceaf0d03686700a94c2
gareth0712 <gareth_0712@hotmail.com>
2020-12-24 15:43:40 +0800

    Create handlerFactory for  some repetitive API controllers

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -3,3 +3,9 @@ module.exports = (fn) => {
     fn(req, res, next).catch(next);
   };
 };
+
+// a = catchAsync(fn)
+// becomes a = (req, res, next) => fn(req, res, next).catch(next)
+// In practice, a = catchAsync((req, res, next) => res.end('Hello world'))
+// becomes a = (req, res, next) => res.end('Hello world').catch(next)
+// During the above process, closure allows fn to maintain its "value" (i.e. the callback function) inside a even tho the catchAsync function has already returned
