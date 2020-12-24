9625506214f0aa67ba8c1e6ca34ae81ab10e1773
gareth0712 <gareth_0712@hotmail.com>
2020-12-24 15:44:54 +0800

    Add updateOne handler for handlerFactory

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -4,6 +4,25 @@ const AppError = require('../utils/appError');
 // A function returning another handler function
 // closure chain
 // exports.a = (b) => (() => {})
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const updatedDoc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedDoc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: updatedDoc,
+      },
+    });
+  });
+
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
@@ -17,6 +36,28 @@ exports.deleteOne = (Model) =>
       data: null,
     });
   });
+
+// exports.updateTour = catchAsync(async (req, res, next) => {
+//   // findByIdAndUpdate method
+//   // 1st: id to identify the doc
+//   // 2nd: what data should be updated
+//   // 3rd: other options: e.g. { new: true } means asking the method to return the updated tour
+//   const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+//     new: true,
+//     runValidators: true,
+//   });
+
+//   if (!updatedTour) {
+//     return next(new AppError('No tour found with that ID', 404));
+//   }
+
+//   res.status(200).json({
+//     status: 'success',
+//     data: {
+//       updatedTour,
+//     },
+//   });
+// });
 
 // exports.deleteTour = catchAsync(async (req, res, next) => {
 //   const tour = await Tour.findByIdAndDelete(req.params.id);
