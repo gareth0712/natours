9c840788d5ab1e779ce74fecfb20a7818521314b
gareth0712 <gareth_0712@hotmail.com>
2020-12-28 10:28:32 +0800

    Add sample data and update import-sample-data.js respectively

--- sample-data/import-sample-data.js
+++ sample-data/import-sample-data.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Tour = require('./models/tourModel');
-const User = require('./models/userModel');
-const Review = require('./models/reviewModel');
+const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
+const Review = require('../models/reviewModel');
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: '../config.env' });
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -22,15 +22,9 @@ mongoose
   .then(() => console.log('DB connection successful!'));
 
 // READ Json file
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf-8')
-);
-const users = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/users.json`, 'utf-8')
-);
-const reviews = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/reviews.json`, 'utf-8')
-);
+const tours = JSON.parse(fs.readFileSync('./tours.json', 'utf-8'));
+const users = JSON.parse(fs.readFileSync('./users.json', 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync('./reviews.json', 'utf-8'));
 
 // Import Data into DB
 const importData = async () => {
